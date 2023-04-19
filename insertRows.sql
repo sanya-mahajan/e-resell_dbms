9381c7725d8d03977519ab4179f902556068d16f
Dhruvv101 <97012531+Dhruvv101@users.noreply.github.com>
2023-04-19 17:06:13 +0530

    Update insertRows.sql

--- insertRows.sql
+++ insertRows.sql
@@ -14,3 +14,26 @@ INSERT INTO PRODUCT VALUES(12, 'Steam iron','Unboxed','zap.com','Appliance',1299
 INSERT INTO PRODUCT VALUES(13, 'Shorts','Like new','ss.com','Clothing',499,2);
 INSERT INTO PRODUCT VALUES(14, 'IPhone 8','128GB','apple.com','Electronics',20999,1);
 INSERT INTO PRODUCT VALUES(15, 'Bagpack','Good','gen.com','Utiity',999,2);
+
+
+--User table values
+INSERT INTO USERS VALUES(1,'Amit','Kumar','akumar1@gmail.com',9874536271);
+INSERT INTO USERS VALUES(2,'Bhavya','Mittal','bmit@yahoo.com',9756381430);
+INSERT INTO USERS VALUES(3,'Chirag','Singla','csingla1@thapar.edu',8966745362);
+INSERT INTO USERS VALUES(4,'Dhananjay','Singh','dsingh@outlook.com',9867856473);
+INSERT INTO USERS VALUES(5,'Eli','Roth','elisgreat1@gmail.com',9987500845);
+INSERT INTO USERS VALUES(6,'Farhan','Qureshi','farhan2@yahoo.com',8900065784);
+INSERT INTO USERS VALUES(7,'Garima','Arora','arima1@gmail.com',9806444837);
+INSERT INTO USERS VALUES(8,'Harpreet','Sodhi','sodhi23@gmail.com',9800756223);
+INSERT INTO USERS VALUES(9,'Ishaan','Soin','ishaan@live.com',9800657899);
+INSERT INTO USERS VALUES(10,'Jai','Dalmotra','jaydal@yahoo.com',8989076453);
+INSERT INTO USERS VALUES(11,'Kalki','Koechlin','kalki@gmail.com',7890453329);
+INSERT INTO USERS VALUES(12,'Lakshay','Gupta','gupay1@gmail.com',8769504956);
+INSERT INTO USERS VALUES(13,'Manan','Singla','singmanan@yahoo.com',9087789564);
+INSERT INTO USERS VALUES(14,'Nupur','Arora','nupur23@gmail.com',7895085647);
+INSERT INTO USERS VALUES(15,'Ojas','Gautam','ojasg@gmail.com',8907564783);
+INSERT INTO USERS VALUES(16,'Paras','Gupta','pgu1@thapar.edu',8721314156);
+INSERT INTO USERS VALUES(17,'Quill','Minar','delhie@gmail.com',6789567483);
+INSERT INTO USERS VALUES(18,'Rajesh','Mehta','rajesh@gmail.com',8975647392);
+INSERT INTO USERS VALUES(19,'Bhavya','Mittal','mittal@gmail.com',7890765678);
+INSERT INTO USERS VALUES(20,'Dhananjay','Singh','singg@gmail.com',8907567498);
