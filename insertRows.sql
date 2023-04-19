d04fcd8d9141d2ececee43d82b97bf643423d44a
Dhruvv101 <97012531+Dhruvv101@users.noreply.github.com>
2023-04-19 16:20:43 +0530

    Update insertRows.sql

---
+++ insertRows.sql
@@ -0,0 +1,16 @@
+--Product table values
+INSERT INTO PRODUCT VALUES(1, 'T-shirt','Good','pic.com','Clothing',299,1);
+INSERT INTO PRODUCT VALUES(2, 'Laptop','Like new','abc.com','Electronics',29999,1);
+INSERT INTO PRODUCT VALUES(3, 'Levis jeans','Good','ssd.com','Clothing',799,1);
+INSERT INTO PRODUCT VALUES(4, 'Lab Coat','Okay','lab.com','Clothing',249,2);
+INSERT INTO PRODUCT VALUES(5, 'Bay-Blade','Excellent','toy.com','Toys',199,1);
+INSERT INTO PRODUCT VALUES(6, 'Wired Mouse','Good','m.in','Electronics',499,5);
+INSERT INTO PRODUCT VALUES(7, 'Keyboard','Nice','k.in','Electronics',1099,1);
+INSERT INTO PRODUCT VALUES(8, 'Monitor','Unboxed','hehe.com','Electronics',7999,1);
+INSERT INTO PRODUCT VALUES(9, 'Tool-set','Like new','tool.in','Utility',2799,1);
+INSERT INTO PRODUCT VALUES(10, 'Let us C++','Year old','book.in','Books',399,1);
+INSERT INTO PRODUCT VALUES(11, 'Harry Potter','2 years old','pic.com','Book',799,2);
+INSERT INTO PRODUCT VALUES(12, 'Steam iron','Unboxed','zap.com','Appliance',1299,1);
+INSERT INTO PRODUCT VALUES(13, 'Shorts','Like new','ss.com','Clothing',499,2);
+INSERT INTO PRODUCT VALUES(14, 'IPhone 8','128GB','apple.com','Electronics',20999,1);
+INSERT INTO PRODUCT VALUES(15, 'Bagpack','Good','gen.com','Utiity',999,2);
